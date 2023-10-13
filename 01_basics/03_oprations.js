7419f988b4b7991127ac1284014a64f16e9062f7
MItesh Godhani <145995120+miteshgodhani8619@users.noreply.github.com>
2023-10-13 10:37:17 +0000

    icons and fonts

---
+++ 01_basics/03_oprations.js
@@ -0,0 +1,14 @@
+let score = 33;
+
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
