5d51f618cbb3c3484438a63cb19167fd1f3320f5
MItesh Godhani <145995120+miteshgodhani8619@users.noreply.github.com>
2023-10-13 11:39:39 +0000

    learn oprations

--- 01_basics/03_oprations.js
+++ 01_basics/03_oprations.js
@@ -1,21 +1,27 @@
-let score = 33;
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
 
 
-console.log(typeof(score));
 
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
 
 
-let isLoggedIn = "";
-
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
 
 
-let someNumber = 33;
 
-let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
+
+
+
+
+
