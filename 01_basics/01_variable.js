fbba6f3da78080f1b025f8bf4c3635d705352b38
MItesh Godhani <145995120+miteshgodhani8619@users.noreply.github.com>
2023-10-13 10:14:26 +0000

    Data Type

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 9546768134;
+let accountEmail = "miteshgodhani85@gmail.com";
+var accountPassword = 987321;
+accountCity = "Jaipur";
+
+accountEmail = "rbgodhani@gmail.com";
+accountPassword = 21212121;
+accountCity = "Surat";
+let accountState;
+
+// accountId = 5  // Not Allowed 
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
